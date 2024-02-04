ce04d70c70553537ffaa7a9b2f2218c91b0dcdc7
oeto2 <oeto2@naver.com>
2024-02-04 23:58:16 +0900

    Feat 메인화면 구성

---
+++ Assets/Scripts/Managers/InfoManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InfoManager : MonoBehaviour
+{
+    public static InfoManager Instance = null;
+    public InputField NicknameInputFeiled;
+    [SerializeField] string playerName;
+
+    public void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            if (Instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    public void SetPlayeName()
+    {
+        playerName = NicknameInputFeiled.text;
+    }
+}
