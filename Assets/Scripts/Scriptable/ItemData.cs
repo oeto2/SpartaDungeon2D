a3a0b83515efa80c8a63fba49a57d8b21d1f1e3d
oeto2 <oeto2@naver.com>
2024-02-05 22:01:45 +0900

    Feat 상점 UI 추가

---
+++ Assets/Scripts/Scriptable/ItemData.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "Item Data", menuName = "Scriptable Object/ItemData", order = int.MaxValue)]
+public class ItemData : ScriptableObject
+{
+    public string name;
+    public string info;
+    public Sprite ItemImage;
+    public int jokeValue;
+    public int hpValue;
+    public int missValue;
+}
+
+
+
+
